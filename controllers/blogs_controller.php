854701d2802486e6c33fd10c7070f01205af3b43
amico <amico8amico@gmail.com>
2016-02-12 11:46:57 +0800

    コントローラのindex()メソッド呼び出し

--- controllers/blogs_controller.php
+++ controllers/blogs_controller.php
@@ -1,3 +1,27 @@
 <?php
-echo 'blogs_controller.phpが呼び出されました！';
+
+	// コントローラのクラスをインスタンス化
+	$controller = new BlogsController();
+
+	// アクション名によって呼び出すメソッドを変える
+	switch ($action) {
+		case 'index':
+			$controller->index();
+			break;
+
+		default:
+			# code...
+			break;
+	}
+
+	/**
+	* コントローラのクラス
+	*/
+	class BlogsController {
+
+		/* 一覧ページを表示 */
+		function index(){
+			echo 'コントローラのindex()が呼ばれました！';
+		}
+	}
 ?>
